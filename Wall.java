196d3b361e3cca4d33de3846a9b9fdc95f988975
Muffii <mzel7@o2.pl>
2024-09-09 15:22:32 +0200

    deleting double looping in Wall methods, adding flatMap version of count

--- Wall.java
+++ Wall.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+//import do wersji z flatMap
+//import java.util.stream.Stream;
 
 public class Wall implements Structure {
 
@@ -8,61 +10,78 @@ public class Wall implements Structure {
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        return blocks.stream().map((block) -> findBlockByColorRecursive(block, color))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst();
+        return findBlockByColorRecursive(blocks, color);
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        List<Block> foundBlocks = new ArrayList<>();
-        for (Block block : blocks) {
-            findBlocksByMaterialRecursive(block, material, foundBlocks);
-        }
-        return foundBlocks;
+        return findBlocksByMaterialRecursive(blocks, material);
     }
 
     @Override
     public int count() {
-        return blocks.stream().mapToInt((block) -> countRecursive(block)).sum();
+        return countRecursive(blocks);
     }
 
     public void addBlock(Block block) {
         blocks.add(block);
     }
 
-    private Optional<Block> findBlockByColorRecursive(Block block, String color) {
-        if (block.getColor().equals(color)) {
-            return Optional.of(block);
-        }
-        if (block instanceof CompositeBlock) {
-            CompositeBlock compositeBlock = (CompositeBlock) block;
-            return compositeBlock.getBlocks().stream()
-                    .map((inner) -> findBlockByColorRecursive(inner, color))
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .findFirst();
+    private Optional<Block> findBlockByColorRecursive(List<Block> blocks, String color) {
+        for (Block block : blocks) {
+            if (block.getColor().equals(color)) {
+                return Optional.of(block);
+            } 
+            if (block instanceof CompositeBlock) {
+                CompositeBlock compositeBlock = (CompositeBlock) block;
+                Optional<Block> result = findBlockByColorRecursive(compositeBlock.getBlocks(), color);
+                if (result.isPresent()) {
+                    return result;
+                }
+            }
         }
         return Optional.empty();
     }
 
-    private void findBlocksByMaterialRecursive(Block block, String material, List<Block> foundBlocks) {
-        if (block.getMaterial().equals(material)) {
-            foundBlocks.add(block);
+    private List<Block> findBlocksByMaterialRecursive(List<Block> blocks, String material) {
+        List<Block> foundBlocks = new ArrayList<>();
+        for (Block block : blocks) {
+            if (block.getMaterial().equals(material)) {
+                foundBlocks.add(block);
+            }
+            if (block instanceof CompositeBlock) {
+                CompositeBlock compositeBlock = (CompositeBlock) block;
+                foundBlocks.addAll(findBlocksByMaterialRecursive(compositeBlock.getBlocks(), material));
+            }
         }
-        if (block instanceof CompositeBlock) {
-            CompositeBlock compositeBlock = (CompositeBlock) block;
-            compositeBlock.getBlocks().forEach((inner) -> findBlocksByMaterialRecursive(inner, material, foundBlocks));
-        }
+        return foundBlocks;
     }
 
-    private int countRecursive(Block block) {
-        if (block instanceof CompositeBlock) {
-            CompositeBlock compositeBlock = (CompositeBlock) block;
-            return compositeBlock.getBlocks().stream().mapToInt((inner) -> countRecursive(inner)).sum();
+    private int countRecursive(List<Block> blocks) {
+        int count = 0;
+        for (Block block : blocks) {
+            count++;
+            if (block instanceof CompositeBlock) {
+                CompositeBlock compositeBlock = (CompositeBlock) block;
+                count += countRecursive(compositeBlock.getBlocks());
+            }
         }
-        return 1;
+        return count;
     }
 
+    // wersja z flatMap
+    // public int count() {
+    //     return (int) blocks.stream()
+    //                        .flatMap(this::flatten)
+    //                        .count();
+    // }
+
+    // private Stream<Block> flatten(Block block) {
+    //     if (block instanceof CompositeBlock) {
+    //         CompositeBlock compositeBlock = (CompositeBlock) block;
+    //         return Stream.concat(Stream.of(block), compositeBlock.getBlocks().stream().flatMap(this::flatten));
+    //     } else {
+    //         return Stream.of(block);
+    //     }
+    // }
 }
